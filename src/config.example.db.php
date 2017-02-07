fdec660b9965c01fedf2de072bc213464bc5177a
Zander Work <zander@zanderwork.com>
2017-02-07 15:27:20 -0800

    added example db config file

---
+++ src/config.example.db.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Zander Work on 2/6/2017 9:43 PM.
+ */
+define("DB_HOST", "db_ip_here");
+define("DB_PORT", 3306); //change port number if need be, 3306 is the default for MySQL
+define("DB_USER", "db_user_here");
+define("DB_PASS", "db_pass_here");
+define("DB_NAME", "db_name_here");
+?>
