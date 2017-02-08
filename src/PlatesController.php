c5b866a523d5f5d26dde4d5bf2642780d9e7409d
Zander Work <zander@zanderwork.com>
2017-02-07 19:04:03 -0800

    wrote a class to wrap Plates in a singleton

---
+++ src/PlatesController.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+define("TEMPLATE_DIR", "templates/");
+
+/**
+ * Created by Zander Work on 2/7/2017 6:35 PM.
+ */
+class PlatesController {
+
+	private static $templates = NULL;
+
+	private function __construct() {
+
+	}
+
+	public static function getInstance() {
+		if (self::$templates === NULL) {
+			self::$templates = new \League\Plates\Engine(TEMPLATE_DIR);
+		}
+		return self::$templates;
+	}
+
+	public static function renderTemplate($template, $args = []) {
+		return self::getInstance()->render($template, $args);
+	}
+
+}
