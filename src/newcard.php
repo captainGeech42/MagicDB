e21ab8a6cec79e9d56498e354c4e544c250d7895
Zander Work <zander@zanderwork.com>
2017-02-09 11:49:56 -0800

    removed card scraping test from newcard

--- src/newcard.php
+++ src/newcard.php
@@ -3,14 +3,7 @@
  * Created by Zander Work on 2/7/2017 8:10 PM.
  */
 
-require_once('Database.php');
 require_once('PlatesController.php');
-require_once('CardScraper.php');
 
-$cardScraper = new CardScraper($_GET['id'], $_GET['set'], CARDTYPE_CREATURE);
-$image = $cardScraper->getCardImageURL();
-
-echo \PlatesController::renderTemplate('newcard', ['image' => $image]);
-echo '<br><br>';
-$cardScraper->scrapCardInfo();
+echo \PlatesController::renderTemplate('newcard');
 ?>
--- src/templates/newcard.tpl.php
+++ src/templates/newcard.tpl.php
@@ -1,7 +1,2 @@
 <?php $this->layout('pageLayout', ['title' => 'MagicDB | Add Card']) ?>
-<!--
-<form action="newcard.php" method="post">
 
-</form>
--->
-<img src="<?=$image?>">
