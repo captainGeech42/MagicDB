e0c7e46f3f9e2e2dd6c7f4fcdefe2d503b12fde9
Zander Work <zander@zanderwork.com>
2017-02-09 18:26:58 -0800

    changed switch to combo if/elif/else because php doesn't allow fall through switch cases (ugh)

--- src/CardScraper.php
+++ src/CardScraper.php
@@ -4,7 +4,7 @@
  * Created by Zander Work on 2/7/2017 11:29 PM.
  */
 
-define('CARDTYPE_BASIC_LAND', 0);
+//define('CARDTYPE_BASIC_LAND', 0); basic lands are parsed the same as lands, no need to differentiate them b/c they are similar
 define('CARDTYPE_LAND', 1);
 define('CARDTYPE_CREATURE', 2);
 define('CARDTYPE_ARTIFACT', 3);
@@ -73,10 +73,6 @@ class CardScraper {
 	}
 
 	public function scrapCardInfo() {
-		if ($this->cardType != 2) {
-			echo 'invalid card type given. only creatures are currently supported. <br>';
-			return false;
-		}
 		$html = $this->getHTML($this->getCardInfoURL());
 		$domDocument = new DOMDocument();
 
@@ -101,100 +97,101 @@ class CardScraper {
 
 						//TODO IMPORTANT: THIS ONLY WORKS FOR CREATURE CARDS RIGHT NOW, EVENTUALLY IT WILL BE EXPANDED TO ALL
 
-						switch($this->cardType) {
-							case CARDTYPE_CREATURE:
-								//get the row
-								$td = $domXpath->query('//td[@valign="top" and @style="padding: 0.5em;" and @width="70%"]', $table);
+						if ($this->cardType == CARDTYPE_LAND ||
+							$this->cardType == CARDTYPE_CREATURE) {
+							//get the row
+							$td = $domXpath->query('//td[@valign="top" and @style="padding: 0.5em;" and @width="70%"]', $table);
 
-								$nameNodeList = $domXpath->query('span[@style="font-size: 1.5em;"]/a', $td->item(0));
-								$this->name = $nameNodeList->item(0)->nodeValue;
+							$nameNodeList = $domXpath->query('span[@style="font-size: 1.5em;"]/a', $td->item(0));
+							$this->name = $nameNodeList->item(0)->nodeValue;
 
-								$infoComboLineNodeList = $domXpath->query('p', $td->item(0));
-								$infoComboLine = $infoComboLineNodeList->item(0)->nodeValue;
+							$infoComboLineNodeList = $domXpath->query('p', $td->item(0));
+							$infoComboLine = $infoComboLineNodeList->item(0)->nodeValue;
 
-								$infoComboLineCommaSplit = explode(',', $infoComboLine);
-								//[0] is name and p/t, [1] is colored mana representation and total mana in parantheses that we don't care about
-								//[0]=Creature — Human Monk */*
-								//[1]=3WW (5)
+							$infoComboLineCommaSplit = explode(',', $infoComboLine);
+							//[0] is name and p/t, [1] is colored mana representation and total mana in parantheses that we don't care about
+							//[0]=Creature — Human Monk */*
+							//[1]=3WW (5)
 
-								$this->mana = explode(' ', trim($infoComboLineCommaSplit[1]))[0];
+							$this->mana = explode(' ', trim($infoComboLineCommaSplit[1]))[0];
 
-								if (strpos($infoComboLineCommaSplit[0], '*') !== false) {
-									//card has indefinite p/t, so we know the length of p/t
-									$this->pt = '*/*';
-									$this->typeline = substr($infoComboLineCommaSplit[0], 0, strlen($infoComboLineCommaSplit[0]) - 4);
-								} else {
-									//card has definite p/t, unknown length
-									$split = explode(' ', $infoComboLineCommaSplit[0]);
-									$this->pt = array_pop($split);
-									$typeline = '';
-									foreach($split as $word) {
-										$typeline .= $word . ' ';
-									}
-									$this->typeline = $typeline;
+							if (strpos($infoComboLineCommaSplit[0], '*') !== false) {
+								//card has indefinite p/t, so we know the length of p/t
+								$this->pt = '*/*';
+								$this->typeline = substr($infoComboLineCommaSplit[0], 0, strlen($infoComboLineCommaSplit[0]) - 4);
+							} else {
+								//card has definite p/t, unknown length
+								$split = explode(' ', $infoComboLineCommaSplit[0]);
+								$this->pt = array_pop($split);
+								$typeline = '';
+								foreach($split as $word) {
+									$typeline .= $word . ' ';
 								}
-								$this->typeline = str_replace('—',  '--', $this->typeline);
+								$this->typeline = $typeline;
+							}
+							$this->typeline = str_replace('—',  '--', $this->typeline);
 
-								$ctextNode = $domXpath->query('p[@class="ctext"]', $td->item(0))->item(0)->nodeValue;
+							$ctextNode = $domXpath->query('p[@class="ctext"]', $td->item(0))->item(0)->nodeValue;
 
-								//split on space
-								//foreach, split on ''
-								//foreach, if there are more than one capital letter, put a <br>x2 in
-								$ctextsplit = explode(' ', $ctextNode);
-								$ctextarraycounter = 0;
+							//split on space
+							//foreach, split on ''
+							//foreach, if there are more than one capital letter, put a <br>x2 in
+							$ctextsplit = explode(' ', $ctextNode);
+							$ctextarraycounter = 0;
 
-								//TODO issue: mulitple keywords on one line don't follow the capitalization assumption:
-								//cardlookup.php?set=isd&id=13
-								//possible solution: have an array of all possible keywords, check if any of them are present in $word
+							//TODO issue: mulitple keywords on one line don't follow the capitalization assumption:
+							//cardlookup.php?set=isd&id=13
+							//possible solution: have an array of all possible keywords, check if any of them are present in $word
 
-								foreach ($ctextsplit as $word) {
-									$capitalCounter = 0;
-									$offset = -1;
-									$previousChar = null;
-									foreach (str_split($word) as $char) {
-										$offset++;
-										if (ctype_upper($char)) {
-											$capitalCounter++;
-										}
-										if ($capitalCounter > 1) {
-											//there have been two capitals since the last inserted line break
-											if ($previousChar != null) {
-												//this isn't the first letter
-												//it shouldn't be possible for this check to fail, but better safe than sorry
-												if ($previousChar !== '-') {
-													//if the last character was a -, we don't want a linebreak,
-													//because Geist-Honored can be one line
-													$ctextsplit[$ctextarraycounter] = substr_replace($word, '<br><br>', $offset, 0);
-												}
+							foreach ($ctextsplit as $word) {
+								$capitalCounter = 0;
+								$offset = -1;
+								$previousChar = null;
+								foreach (str_split($word) as $char) {
+									$offset++;
+									if (ctype_upper($char)) {
+										$capitalCounter++;
+									}
+									if ($capitalCounter > 1) {
+										//there have been two capitals since the last inserted line break
+										if ($previousChar != null) {
+											//this isn't the first letter
+											//it shouldn't be possible for this check to fail, but better safe than sorry
+											if ($previousChar !== '-') {
+												//if the last character was a -, we don't want a linebreak,
+												//because Geist-Honored can be one line
+												$ctextsplit[$ctextarraycounter] = substr_replace($word, '<br><br>', $offset, 0);
 											}
-											$capitalCounter = 0;
 										}
-										if ($previousChar === '.') {
-											//we hit the end of a sentence. it is most likely now a new line
-											$ctextsplit[$ctextarraycounter] = substr_replace($word, '<br><br>', $offset, 0);
-
-											//TODO period inside a parenthese suoldn't trigger a new line.
-											//Disregarding this example is an instant, this may be an issue:
-											//cardlookup.php?set=isd&id=13
-										}
-										$previousChar = $char;
+										$capitalCounter = 0;
 									}
-									$ctextarraycounter++;
-								}
+									if (strcmp($previousChar, '.') === 0 && strcmp($char, ')') !== 0) {
+										//we hit the end of a sentence. it is most likely now a new line (unless the next character is a close paren
+										$ctextsplit[$ctextarraycounter] = substr_replace($word, '<br><br>', $offset, 0);
 
-								//recombine the card text
-								foreach ($ctextsplit as $word) {
-									$this->cardText .= $word . ' ';
+										//TODO period inside a parenthese suoldn't trigger a new line.
+										//Disregarding this example is an instant, this may be an issue:
+										//cardlookup.php?set=isd&id=13
+									}
+									$previousChar = $char;
 								}
+								$ctextarraycounter++;
+							}
 
-								//check if we have an empty card text
-								if (strcmp($this->cardText, ' ') === 0) {
-									$this->cardText = '[no cardtext]';
-								}
-								break;
-							case CARDTYPE_ARTIFACT:
-								
-								break;
+							//recombine the card text
+							foreach ($ctextsplit as $word) {
+								$this->cardText .= $word . ' ';
+							}
+
+							//check if we have an empty card text
+							if (strcmp($this->cardText, ' ') === 0) {
+								$this->cardText = '[no cardtext]';
+							}
+						}
+						else if ($this->cardType == CARDTYPE_ARTIFACT) {
+							
+						} else {
+							echo 'card type not supported yet...';
 						}
 
 						//we did the only table we care about, no need to continue the loop
